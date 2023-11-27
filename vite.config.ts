114b2067a9d1ea43e4f03058103d95bc7fae8082
Dmitry.Astashin <Dmitry.Astashin@russianpost.ru>
2023-11-28 01:03:48 +0300

    deploy vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// ty for vite deploy https://github.com/ErickKS/vite-react-router
 export default defineConfig({
   plugins: [react()],
+  base: "/astashin-resume/"
 })
